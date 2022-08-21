9de78d512457b44ed2eb149a882b7b766adee5ca
Reggie899 <regina.schmidt1212@gmail.com>
2022-08-22 00:31:47 +0200

    update

--- about.js
+++ about.js
@@ -1,5 +1,7 @@
 let p = document.getElementById("p");
 let p2 = document.getElementById("p2");
+let p3 = document.getElementById("p3");
+let p4 = document.getElementById("p4");
 let wordButton = document.getElementById("wordButton");
 let wordButton2 = document.getElementById("wordButton2");
 let button = document.getElementById("bionicButton");
@@ -11,11 +13,27 @@ function seeResults() {
     .then((res) => (p2.innerHTML = res.data));
 };
 
+function seeResults2() {
+    axios
+    .get("https://bionic-reading-backend.herokuapp.com/two")
+      // .then(res => console.log("here", res.data))
+      .then((res) => (p3.innerHTML = res.data));
+  };
+
+  function seeResults3() {
+    axios
+    .get("https://bionic-reading-backend.herokuapp.com/three")
+    //   .then(res => console.log("here", res.data))
+      .then((res) => (p4.innerHTML = res.data));
+  };
+
 function bionicReading(event) {
     event.preventDefault();
   // var element = document.getElementById("myDIV");
   p.classList.toggle("mystyle");
   p2.classList.toggle("mystyle2");
+  p3.classList.toggle("mystyle2");
+  p4.classList.toggle("mystyle2");
   wordButton.classList.toggle("wordButton");
   wordButton2.classList.toggle("wordButton2");
   button.classList.toggle("buttonStyle2");
@@ -25,5 +43,8 @@ const test = () => {
     alert("mmm");
 }
 seeResults();
+seeResults2();
+seeResults3();
+
 
 button.addEventListener("click", bionicReading);
